7d4036bce32c7acbe1d2c573be8273d7348e6bf2
Kenan Ergül <k.erguel6700@gmx.de>
2025-04-23 20:38:58 +0200

    1.3.22 Login Probleme

--- init_users.sql
+++ init_users.sql
@@ -2,7 +2,7 @@
 DELETE FROM users;
 
 -- Füge Admin-Benutzer hinzu
-INSERT INTO users (username, password, is_admin) VALUES ('admin', 'admin123', b'1');
+INSERT INTO users (username, password, is_admin) VALUES ('admin', 'pRnf+TXORKJefRH6x9HcfOyIxKEetJvxAqZc10GgB/bxuzFtjHMtSLqaPMYAU9zelkfUwAaP8S0QnclBkbgVJQ==', b'1');
 
 -- Füge Wissenschaftler hinzu
-INSERT INTO users (username, password, is_admin) VALUES ('scientist', 'scientist123', b'0'); 
+INSERT INTO users (username, password, is_admin) VALUES ('scientist', 'Rp8KPc67CVZdkW/GdjvnQuFNfaBxRBSnYd8wPw4cQyWE/+Ef60ujGAjp1EoO3M5YdUjvp0YPw2dpurT++YRCsg==', b'0'); 
