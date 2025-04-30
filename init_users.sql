38e849a92d3a60111f35f05d229c181298d82ef5
Kenan Ergül <k.erguel6700@gmx.de>
2025-04-30 20:53:29 +0200

    1.5.39 Aktualisiere init_users.sql: Ändere das Passwort für den Benutzer 'science1' auf einen neuen Hash-Wert.

--- init_users.sql
+++ init_users.sql
@@ -5,4 +5,4 @@ DELETE FROM users;
 INSERT INTO users (username, password, is_admin) VALUES ('admin', 'pRnf+TXORKJefRH6x9HcfOyIxKEetJvxAqZc10GgB/bxuzFtjHMtSLqaPMYAU9zelkfUwAaP8S0QnclBkbgVJQ==', 1);
 
 -- Füge Wissenschaftler hinzu
-INSERT INTO users (username, password, is_admin) VALUES ('science1', 'hbhYGATKF5tsfqaK7ycsZMsZBMVZNXwwxKQ0/rj373bUlV7e6tIlF7tc4VvD5y47erAJRRrjPtuxTSfHtCMdow==', 0); 
+INSERT INTO users (username, password, is_admin) VALUES ('science1', 'UwDrSMEU17lYJf28Yh6PskN8iLD96coRnzl7FntHSIu5bGL8fgQIkv69hgaE7sarlgik5E2W28WEsPSAun2OWg==', 0); 
